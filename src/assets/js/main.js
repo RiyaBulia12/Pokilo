2fca0040bfd0eeea95b5cef9bb92a6c7f5a92330
Apple <riyabulia12@gmail.com>
2022-08-31 19:45:53 +0530

    Add dynamic likes to the homepage

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,30 @@
 import '../css/main.css';
-import { getPoke } from './api.js';
+
+import { getLikes, getPoke } from './api.js';
 import { viewPoke } from './comments.js';
 import { importImages } from './functions.js';
+import updateLikes from './likes.js';
 
 const images = importImages(require.context('../img', false, /\.(png|jpe?g|svg)$/));
 const grid = document.querySelector('.poke-grid');
-const createGrid = (item, id) => {
+const createGrid = (item, id, likes) => {
+  // =======
+  // import {  getLikes, getPoke } from './api.js';
+  // import { updateLikes } from './likes';
+  // // import { loadLikes } from './likes.js';
+  // function importImages(r) {
+  //   const images = {};
+  //   r.keys().forEach((item) => {
+  //     images[item.replace('./', '')] = r(item);
+  //   });
+  //   return images;
+  // }
+
+  // const images = importImages(require.context('../img', false, /\.(png|jpe?g|svg)$/));
+  // const grid = document.querySelector('.poke-grid');
+
+  // const createGrid = (item, id, likes) => {
+  // >>>>>>> Stashed changes
   grid.innerHTML += `
     <div class="col-3 ">
       <div class="image">
@@ -13,7 +32,8 @@ const createGrid = (item, id) => {
       </div>
       <div class="desc">
         <h3>${item.name}</h3>
-        <img src="${images['like.png']}" alt="Like Icon" class="icon"/>
+        <i class="bi bi-heart" id="${item?.name}"></i>
+        <span class="like-counts" id="like-${item?.name}">${likes}</span>
       </div>
       <div class="btn-container">
         <button type="button" class="btn btn-warning comment-btn" id="${id}" data-bs-toggle="modal" data-bs-target="#projectModal">Comment</button>
@@ -23,11 +43,20 @@ const createGrid = (item, id) => {
 };
 
 const loadPoke = async () => {
-  let id = 1;
   const getPokemon = await getPoke();
-  getPokemon.forEach((item) => {
-    id += 1;
-    createGrid(item, id);
+  const getLikeList = await getLikes();
+  getPokemon.forEach((item, id) => {
+    const like = getLikeList[id]?.likes;
+    const pokeId = +item.url.split('/').filter(Boolean).pop();
+    createGrid(item, pokeId, like);
+
+    const likeIcon = document.querySelectorAll('.bi-heart');
+    likeIcon.forEach((item) => {
+      item.addEventListener('click', (e) => {
+        updateLikes(e);
+      });
+    });
+
     const commentBtns = document.querySelectorAll('.comment-btn');
     commentBtns.forEach((comment) => {
       comment.onclick = (event) => {
